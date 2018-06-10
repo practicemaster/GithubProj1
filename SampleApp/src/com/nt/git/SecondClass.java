a5fcc7960eb24dfdacc573b4a09a969b4e4c5d37
ushakiran <ushakiran@uday>
2018-06-10 19:33:21 +0530

    reply to praveen

--- SampleApp/src/com/nt/git/SecondClass.java
+++ SampleApp/src/com/nt/git/SecondClass.java
@@ -5,5 +5,8 @@ public class SecondClass {
 	public static void main(String[] args) {
 		System.out.println("this is the second class");
 		System.out.println("uday");
+		for(int i=0;i<=5;i++) {
+			System.out.println("check your code praveen");
+		}
 	}
 }
