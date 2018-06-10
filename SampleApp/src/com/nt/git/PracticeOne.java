3f7e603f3d3b37824806897b7a46caa2360b328a
ushakiran <ushakiran@192.168.1.15>
2018-06-10 14:00:56 +0530

    last minute

--- SampleApp/src/com/nt/git/PracticeOne.java
+++ SampleApp/src/com/nt/git/PracticeOne.java
@@ -13,5 +13,6 @@ public class PracticeOne {
 		}
 		System.out.println("after loop");
 		System.out.println("i made change in code");
+		System.out.println("this is the last minute of our worinkg day");
 	}
 }
