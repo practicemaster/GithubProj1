38fafe442a1cd4bf76af060fab1dfd464c078aab
ushakiran <ushakiran@uday>
2018-06-10 18:52:45 +0530

    hihi

--- SampleApp/src/com/nt/git/PracticeTwo.java
+++
@@ -1,9 +0,0 @@
-package com.nt.git;
-
-public class PracticeTwo {
-
-	public static void main(String[] args) {
-		System.out.println("this is my 2nd practice class");
-	}
-
-}
---
+++ SampleApp/src/com/nt/git/SecondClass.java
@@ -0,0 +1,8 @@
+package com.nt.git;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		System.out.println("this is the second class");
+	}
+}
