eabe2fff5f672240c341450e1da2dd940cce2456
CG-DTE <CG-DTE@192.168.1.16>
2018-06-10 13:28:56 +0530

    second commit

--- SampleApp/src/com/nt/git/PracticeOne.java
+++ SampleApp/src/com/nt/git/PracticeOne.java
@@ -3,5 +3,12 @@ package com.nt.git;
 public class PracticeOne {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		//rewriting hello 
+		for(int i = 1;i<=10;i++)
+		{
+			System.out.println(i);
+			
+			
+		}
 	}
 }
